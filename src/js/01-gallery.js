83f61eb089d8dbe5c3c000f65247b1f8464fe249
SVolochandrey <volochan.andrey@gmail.com>
2023-02-16 08:43:59 +0200

    done 1st and 2nd tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,32 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+const markupEl = createImageGalleryMarkup(galleryItems);
+galleryList.innerHTML = markupEl;
+
+function createImageGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}" 
+  alt="${description}" 
+  />
+</a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from "@vimeo/player";
+import  throttle from "lodash.throttle";
 
+const iFrame = document.querySelector('#vimeo-player');
+const player = new Player(iFrame);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+pageReload();
+
+const onPlay = function ({seconds}) {
+localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function pageReload(event){
+const storageValue = localStorage.getItem(LOCALSTORAGE_KEY);
+if (!storageValue){
+return;
+}
+player.setCurrentTime(storageValue);
+}
